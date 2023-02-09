559fe0cdc37766b7388f15e937b98aaba14cdac1
nehihz <zhihenkew@gmail.com>
2023-02-09 19:30:48 +0800

    commit R files

---
+++ r_studio/r_studio/integers_r.R
@@ -0,0 +1,12 @@
+#printing integers from 1 to 10 using a for loop
+for (i in c(1:10)) {
+    print(i)
+}
+
+#printing integers from 1 to 10 using a while loop
+j <- 1
+while (j <= 10) {
+  print(j)
+  j <- j + 1
+}
+
