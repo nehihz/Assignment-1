c596b16e9b7ca46aa95803239c03d313fbde5a68
nehihz <zhihenkew@gmail.com>
2023-02-09 19:32:45 +0800

    commit python files

---
+++ spyder/integers_python.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 18:49:50 2023
+
+@author: zhihen
+"""
+
+#printing intergers from 1 to 10 without a loop
+print(1,2,3,4,5,6,7,8,9,10)
+
+#printing integers from 1 to 10 with a for loop
+for i in range(1,11):
+    print(i)
+
+#printing integers from 1 to 10 using a while loop
+j = 1
+while (j <= 10):
+    print (j)
+    j = j + 1
